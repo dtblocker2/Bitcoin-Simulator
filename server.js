4cda2f7eb2f27e1a8dfcac960a140527805e9f22
dhruva <dk30nov2005@gmail.com>
2025-06-05 17:39:26 +0530

    The project is alsmost done next time I will add decoration by css and also some minor error handling such as non allowance of negative balance, user haptics like alerts etc.

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+//declaration of modules
 const axios = require('axios');
+const path = require('path');
 
+//declaration of important constants
 const apikey = '0cdfb6ef-cc38-4a44-8c5f-68c5fd4c096c';
 const symbol = 'BTC';
 
+//declaration of some functions
 async function fetch_btc_price() {
   try {
     const response = await axios.get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=${symbol}`, {
@@ -11,7 +15,7 @@ async function fetch_btc_price() {
       },
     });
     const price = response.data.data.BTC.quote.USD.price;
-    console.log(price);
+    //console.log(price);
     return price;
   } catch (error) {
     console.error('Error fetching price:', error.message);
@@ -19,7 +23,7 @@ async function fetch_btc_price() {
   }
 }
 
-// server.js
+// server.js setup
 const express = require('express');
 const cors = require('cors');
 
@@ -30,11 +34,20 @@ app.set('view engine', 'ejs');
 app.get('/', async (req, res) => {
   const BTC_curr_price = await fetch_btc_price(); // used await to remove error or fetch_btc_price function return object and nit string
   try {
+    // Serve static files from the 'public' directory
+      app.use(express.static(path.join(__dirname, 'public')));
+      //above will be executed only after getting value of BTC to avoid blank response
       res.render('index', { BTC_curr_price });
     } catch (error) {
       console.error('Error fetching BTC price:', error.message);
       res.render('index', { BTC_curr_price: 'Error fetching price' });
     }
+});
+
+//live update code in script.js document
+app.get('/api/btc-price', async (req,res) => {
+  const price = await fetch_btc_price();
+  res.json({price})
 });
 
 app.listen(3000, () => {
